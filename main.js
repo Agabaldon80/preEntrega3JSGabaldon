7642b9b919b63ba6a1ef3f1098ec6001af0125ce
Andres Gabaldon <andres.gabaldon@gmail.com>
2023-09-08 21:40:55 -0300

    rehice array agregue querySelector event listener json parse y storage, cree formulario y separe funciones

--- main.js
+++ main.js
@@ -1,129 +1,156 @@
 /* Algoritmo para nota final de estudiantes ingresados */
 
-class Estudiante {
-    constructor(id, nombre, nota) {
-      this.id = id;
-      this.nombre = nombre;
-      this.nota = nota;
-    }
-  }
+
+let estudiantesArray = [];
+        let tabla = document.querySelector('#tabla');
+        let promNota = document.querySelector('#promNota');
+        let promEdad = document.querySelector('#promEdad');
+        let maxNota = document.querySelector('#maxNota');
+        let minNota = document.querySelector('#minNota');
+        let formulario = document.querySelector('#cargar');
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const estudiantesAlmacenados = localStorage.getItem('Estudiantes');
+
+            if(estudiantesAlmacenados) {
+                let estudiantes = JSON.parse(estudiantesAlmacenados);
+                //estudiantesArray.push(JSON.parse(estudiantesAlmacenados));
+                for(let i = 0; i < estudiantes.length; i++){
+                    estudiantesArray.push(estudiantes[i]);
+                    tabla.innerHTML += `
+                                    <tr>
+                                        <td>${estudiantes[i].id}</td>
+                                        <td>${estudiantes[i].nombre}</td>
+                                        <td>${estudiantes[i].apellido}</td>
+                                        <td>${estudiantes[i].edad}</td>
+                                        <td>${estudiantes[i].nota}</td>
+                                        <td>${estudiantes[i].mencion}</td>
+                                    </tr>
+                                ` ;
+                }
+                promNota.innerHTML = `${Math.round(promedio(estudiantesArray,'nota'))}`;
+                promEdad.innerHTML = `${Math.round(promedio(estudiantesArray,'edad'))}`;
+                maxNota.innerHTML = `${notaMax()}`;
+                minNota.innerHTML = `${notaMin()}`;
+            }
   
-  const estudiantesArray = [];
-  
-  /** Mensajes para el usuario */
-  
-  function mensaje(texto) {
-    switch (texto) {
-      case "bienvenida":
-        alert(
-          "Bienvenido al calculador de notas Eleusis, el cual le permite determinar el promedio de los estudiantes en su curso y si el mismo ha obtenido una mención. Para conocer el sistema de notación y las posibles menciones, haga por favor click en 'aceptar'"
-        );
-        break;
-  
-      case "instruccion":
-        alert(
-          "El sistema de notación es de 1 hasta 20 puntos. Si el promedio es de 18, 19 o 20 puntos, el curso logra la mención 'Sobresaliente'; de 16 0 17 puntos, logra la mención 'Excelente'; de 14 o 15 puntos logra la mención 'Correcto'. Una nota menor a 14 no obtiene mención. Haga click en 'aceptar' para continuar y siga las instrucciones"
-        );
-        break;
-  
-      case "despedida":
-        alert("Gracias por usar el calculador de notas Eleusis. Vuelva pronto");
-        break;
-    }
-  }
-  
-  function mencion(texto, curso, promedioNota) {
-    let string = "";
-    estudiantesArray.forEach((alumnos) => {
-      string += `${alumnos.id}.- ${alumnos.nombre} tiene una nota de: ${alumnos.nota} puntos.\n`;
-    });
-  
-    switch (texto) {
-      case "sobresaliente":
-        alert(
-          `El promedio del curso ${curso} es ${promedioNota} puntos y logra la mención 'Sobresaliente'\n${string}`
-        );
-        break;
-  
-      case "excelente":
-        alert(
-          `El promedio del curso ${curso} es ${promedioNota} puntos y logra la mención 'Excelente'\n${string}`
-        );
-        break;
-      case "correcto":
-        alert(
-          `El promedio del curso ${curso} es ${promedioNota} puntos y logra la mención 'Correcto'\n${string}`
-        );
-        break;
-  
-      case "sin mencion":
-        alert(
-          `El promedio del curso ${curso} es ${promedioNota} puntos y no obtiene mención\n${string}`
-        );
-        break;
-    }
-  }
-  
-  function Notas() {
-    mensaje("bienvenida");
-    mensaje("instruccion");
-  
-    let curso = prompt(
-      "Ingrese el nombre del curso cuyo promedio desea calcular"
-    );
-  
-    let estudiantes = parseInt(
-      prompt(
-        `Ingrese la cantidad de estudiantes en el curso ${curso} y haremos el promedio de sus notas. Atención: se espera un numero entero superior a 0`
-      )
-    );
-  
-    let sumaNotas = 0;
-  
-    while (estudiantes < 1 || isNaN(estudiantes)) {
-      estudiantes = parseInt(
-        prompt(
-          `Ingrese la cantidad de estudiantes en el curso ${curso} y haremos el promedio de sus notas. Atención: se espera un numero entero superior a 0`
-        )
-      );
-    }
-  
-    for (let i = 1; i <= estudiantes; i++) {
-      let nota = parseInt(
-        prompt(
-          `Ingrese la nota del estudiante número ${i}. Recuerde que ésta debe ser un número entero entre 1 y 20`
-        )
-      );
-  
-      while (nota < 1 || nota > 20 || isNaN(nota)) {
-        nota = parseInt(
-          prompt(
-            `Ingrese la nota del estudiante número ${i}. Recuerde que ésta debe ser un número entero entre 1 y 20`
-          )
-        );
-      }
-  
-      sumaNotas += nota;
-  
-      estudiantesArray.push(new Estudiante(i, `El estudiante ${i}`, nota));
-    }
-  
-    const promedioNota = sumaNotas / estudiantes;
-  
-    if (promedioNota <= 20 && promedioNota >= 18) {
-      mencion("sobresaliente", curso, promedioNota);
-    } else if (promedioNota < 18 && promedioNota >= 16) {
-      mencion("excelente", curso, promedioNota);
-    } else if (promedioNota < 16 && promedioNota >= 14) {
-      mencion("correcto", curso, promedioNota);
-    } else {
-      mencion("sin mencion", curso, promedioNota);
-    }
-    mensaje("despedida");
-    return Notas;
-  }
-  
-  Notas();
-  
-  
-  
+        });
+        
+        class Estudiante {
+            constructor(id, nombre, apellido, edad, nota) {
+                this.id = id;
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.edad = edad;
+                this.nota = nota;
+                this.mencion = mencion;
+            }
+        }
+
+        formulario.addEventListener('submit', function(event) {
+            event.preventDefault();
+            cargaEstudiante();
+        });
+        
+        function cargaEstudiante(){
+            let contador = counter();
+            let id = contador++,
+                nombre = document.querySelector('#nombre').value,
+                apellido = document.querySelector('#apellido').value,
+                edad = parseInt(document.querySelector('#edad').value),
+                nota = parseFloat(document.querySelector('#nota').value);
+                mencion = mencionTipo(nota);
+
+                if(isNaN(nota) || nota < 1 || nota > 20 || isNaN(edad) || edad < 18){
+                    if(isNaN(nota) || nota < 1 || nota > 20){
+                        alert(`Por favor, ingresa un número válido entre 1 y 20 en el campo 'nota'.`);
+                        document.querySelector('#nota').value = 0;
+                        document.querySelector('#nota').classList.add('red-border');
+                        return
+                    } else if(isNaN(edad) || edad < 18) {
+                        alert(`Por favor, ingresa una edad válida en el campo 'edad' (no menor a 18 años).`);
+                        document.querySelector('#edad').value = 0;
+                        document.querySelector('#edad').classList.add('red-border');
+                        return
+                    }
+                } else {
+                    estudiantesArray.push(new Estudiante(id,nombre,apellido,edad,nota,mencion));
+                    let listaEstudiantes = JSON.stringify(estudiantesArray);
+                    localStorage.setItem('Estudiantes', listaEstudiantes);
+                    console.log(estudiantesArray);
+                    tabla.innerHTML += `
+                                <tr>
+                                    <td>${id}</td>
+                                    <td>${nombre}</td>
+                                    <td>${apellido}</td>
+                                    <td>${edad}</td>
+                                    <td>${nota}</td>
+                                    <td>${mencion}</td>
+                                </tr>
+                            ` ;
+
+                    document.querySelector('#nombre').value = '';
+                    document.querySelector('#apellido').value = '';
+                    document.querySelector('#edad').value = 0;
+                    document.querySelector('#edad').classList.remove('red-border');
+                    document.querySelector('#nota').value = 0;
+                    document.querySelector('#nota').classList.remove('red-border');
+
+                    promNota.innerHTML = `${Math.round(promedio(estudiantesArray,'nota'))}`;
+                    promEdad.innerHTML = `${Math.round(promedio(estudiantesArray,'edad'))}`;
+                    maxNota.innerHTML = `${notaMax()}`;
+                    minNota.innerHTML = `${notaMin()}`;
+                }           
+        }
+
+        function promedio(array,dato) {
+            if (array.length === 0) {
+                return 0; // Si no hay estudiantes, el promedio es 0.
+            }
+
+            let notas = 0;
+            let edades = 0;
+            let cantidad = counter();
+
+            array.forEach(estudiante => {
+            dato == 'nota'? notas += estudiante.nota : edades += estudiante.edad;
+            });            
+            
+            if(dato == 'nota'){
+                return notas / cantidad;
+            } else { 
+                return edades / cantidad;
+            }
+        }
+
+        function notaMax(){
+            const maximoValor = estudiantesArray.reduce((max, estudiante) => {
+                return (estudiante.nota > max) ? estudiante.nota : max;
+            }, estudiantesArray[0].nota);
+            return maximoValor;
+        }
+
+        function notaMin(){
+            const minValor = estudiantesArray.reduce((min, estudiante) => {
+                return (estudiante.nota < min) ? estudiante.nota : min;
+            }, estudiantesArray[0].nota);
+            return minValor;
+        }
+
+        function mencionTipo(nota){
+            switch(true) {
+                case (nota >= 18):
+                    return 'Sobresaliente';
+                case (nota >= 16):
+                    return 'Excelente';
+                case (nota >= 14):
+                    return 'Correcto';
+                case (nota < 14):
+                    return '--';
+            }
+        }
+
+        function counter() {
+            let elementosNoVacios = estudiantesArray.filter(elemento => elemento !== undefined && elemento !== null);
+            return elementosNoVacios.length;
+        }
